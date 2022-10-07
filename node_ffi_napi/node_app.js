c7650383132a90415a52b752431b94fb8345b2e4
Thomas Min <thomas.min.git@gmail.com>
2022-10-07 20:50:47 +0900

    feat: support mac for node

--- node_ffi_napi/node_app.js
+++ node_ffi_napi/node_app.js
@@ -1,16 +1,25 @@
 var ffi = require("ffi-napi");
+var process = require('node:process');
 
 const use32bit = true;
 
 var dllPath;
-if (use32bit) {
-  dllPath = "..\\rust_lib\\target\\i686-pc-windows-msvc";
-} else {
-  dllPath = "..\\rust_lib\\target\\x86_64-pc-windows-msvc";
+if (process.platform === 'darwin') {
+  dllPath = '../rust_lib/target/release/librust_lib.dylib'
+} else if (process.platform === 'win32') {
+  if (use32bit) {
+    dllPath = "..\\rust_lib\\target\\i686-pc-windows-msvc\\release\\rust_lib.dll";
+  } else {
+    dllPath = "..\\rust_lib\\target\\x86_64-pc-windows-msvc\\release\\rust_lib.dll";
+  }
 }
 
-var librust = ffi.Library(`${dllPath}\\release\\rust_lib.dll`, {
-  add: ["int", ["int", "int"]],
-});
+if (dllPath) {
+  var librust = ffi.Library(dllPath, {
+    add: ["int", ["int", "int"]],
+  });
 
-console.log(librust.add(1, 2));
+  console.log(librust.add(1, 2));
+} else {
+  console.error('failed to get dll path.');
+}
